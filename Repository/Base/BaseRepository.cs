94cd3d494ddb661d29d7ac291048fe1c7c4ad8a0
Peter Sviryd <Peter.blr@yandex.ru>
2022-01-27 23:23:37 +0300

    added base interface and implementation

---
+++ Repository/Base/BaseRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppMovie.Data;
+
+namespace WebAppMovie.Repository.Base
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class, new()
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BaseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(T item) => await _context.Set<T>().AddAsync(item);
+
+        public async Task DeleteAsync(int id)
+        {
+            var item = await _context.Set<T>().FindAsync(id);
+
+            if (item != null)
+            {
+                _context.Set<T>().Remove(item);
+            }
+
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
+
+        public async Task<T> GetByIdAsync(int id) => await _context.Set<T>().FindAsync(id);
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T newItem)
+        {
+            _context.Entry(newItem).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Repository/Base/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppMovie.Repository.Base
+{
+    public interface IBaseRepository<T> where T : class, new()
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T item);
+        Task UpdateAsync(T newItem);
+        Task DeleteAsync(int id);
+        Task SaveAsync();
+    }
+}
