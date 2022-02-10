c503c1f464a68b1f8484c38fe5802f1c34939f48
Peter Sviryd <Peter.blr@yandex.ru>
2022-02-10 21:27:54 +0300

    opened views by register

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebAppMovie.Data;
+
+[assembly: HostingStartup(typeof(WebAppMovie.Areas.Identity.IdentityHostingStartup))]
+namespace WebAppMovie.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
